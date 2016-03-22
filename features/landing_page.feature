ca4307f63caec6e100cba73fda60d400cc47e409
Colin Frankish <colin.frankish@bcsg.com>
2016-03-22 17:35:33 +0000

    First routes added for listing players

--- features/landing_page.feature
+++ features/landing_page.feature
@@ -3,3 +3,10 @@ Feature: Landing Page
 Scenario: User arrives on the landing page
   Given I am on the landing page
   Then I should see 'Badders League'
+  And I should see the 'login' button
+  And I should see the 'Players' button
+
+Scenario: User wants to see the list of players
+  Given I am on the landing page
+  And I click the 'Players' button
+  Then I should go to the list of players 
--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -5,3 +5,19 @@ end
 Then(/^I should see 'Badders League'$/) do
   expect(page).to have_content 'Badders League'
 end
+
+Then(/^I should see the 'login' button$/) do
+  expect(page).to have_content 'Login'
+end
+
+Then(/^I should see the 'Players' button$/) do
+  expect(page).to have_content 'Players'
+end
+
+Given(/^I click the 'Players' button$/) do
+  click_link 'Players'
+end
+
+Then(/^I should go to the list of players$/) do
+  expect(page).to have_content 'All Players'
+end
