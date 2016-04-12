45117e8631a9ed225a542744e511a72c05ed757f
Colin Frankish <colin.frankish@bcsg.com>
2016-04-12 17:28:50 +0100

    Created basic CRUD functions and views for players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,12 +12,33 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     if @player.save
-      redirect_to "/players"
+      redirect_to @player
     else
       render "new"
     end
   end
 
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    Player.find(params[:id]).destroy
+    redirect_to players_path
+  end
 
   private
 
